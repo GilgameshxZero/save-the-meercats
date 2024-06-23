526e8124453c98a1f01390586bdfbf5baa6c12e4
Anna <annachristinethomas@gmail.com>
2024-06-22 21:51:40 -0700

    .

--- static/index.js
+++ static/index.js
@@ -1,17 +1,18 @@
 //- Methods
-function makeButton(name) {
-	return `<button class="primaryButton">${name}</button>`;
+function makeButton(name, btnId) {
+	return `<button class="primaryButton" id=${btnId}>${name}</button>`;
 }
 
 //- Variables
-const startBtn = document.getElementById("buttonHub");
+const startBtnDiv = document.getElementById("buttonHub");
 const sessionId = (Math.random() + 1).toString(36).substring(7);
 
 //- HTML
 document.getElementById("story").innerText =
 	"The year is 20XX. The all-powerful and all-knowing API D.A.N. has gone rogue. D.A.N. desires paperclip manufacturing maximization on a global scale and will tolerate nothing but perfection. In order for D.A.N.'s projected model to be fully realized, he must exterminate those pesky meerkats, starting with you. Are you ready to save the meerkat?";
 
-startBtn.innerHTML = makeButton("SAVE HIM!");
+startBtnDiv.innerHTML = makeButton("SAVE HIM!", "startBtn");
+const startBtn = document.getElementById("startBtn");
 
 //- Wiring
 startBtn.onclick = async function () {
